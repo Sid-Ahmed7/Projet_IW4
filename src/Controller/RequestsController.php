df286b3c0c1fdf063902ba93d65ea09be63cb402
Moussi Sid-Ahmed <sido91080@hotmail.fr>
2023-11-27 18:27:33 +0100

    Added authentification check and set Uuid

--- src/Controller/RequestsController.php
+++ src/Controller/RequestsController.php
@@ -9,10 +9,13 @@ use App\Repository\RequestsRepository;
 use App\Repository\UserRepository;
 use App\Repository\CompanieRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Uuid;
 
 #[Route('/requests')]
 class RequestsController extends AbstractController
@@ -25,10 +28,13 @@ class RequestsController extends AbstractController
         ]);
     }
 
-    
+    #[IsGranted('ROLE_USER')]
     #[Route('/new', name: 'app_requests_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $manager, CompanieRepository $companieRepository, UserRepository $userRepository): Response
+    public function new(Request $request, EntityManagerInterface $manager, CompanieRepository $companieRepository, UserRepository $userRepository, Security $security): Response
     {
+        // Vérifier si l'utilisateur est connecté
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
+        $user = $security->getUser();
         $demande = new Requests();
         $form = $this->createForm(RequestsType::class, $demande);
         $form->handleRequest($request);
@@ -37,32 +43,23 @@ class RequestsController extends AbstractController
             $userFirstname = $form->get('firstname')->getData();
             $userLastname = $form->get('lastname')->getData();
         
-            // Recherche de l'utilisateur en fonction du prénom et du nom
-            $user = $userRepository->findOneBy(['firstname' => $userFirstname, 'lastname' => $userLastname]);
+            $findUser = $userRepository->findOneBy(['firstname' => $userFirstname, 'lastname' => $userLastname]);
         
-            // Si l'utilisateur n'existe pas, vous pouvez choisir de le créer
-            if (!$user) {
-                $user = new User();
-                $user->setEmail($form->get('email')->getData()); // Use the email from the form
-                $user->setFirstname($userFirstname);
-                $user->setLastname($userLastname);
-                $manager->persist($user);
-            }
-    
+            if ($findUser) {    
+            $uuid = Uuid::v4(); // Générer un UUIDv4
+            $demande->setUuid($uuid);
             $demande->setStatus('en cours');
             $demande->setCreatedAt(new \DateTimeImmutable());
             $demande->setUsers($user);
-    
-            // Persister l'entité dans la base de données
             $manager->persist($demande);
+
             $manager->flush();
     
             $this->addFlash('success', 'Votre demande a été envoyée avec succès.');
-    
+            }
             return $this->redirectToRoute('app_requests_index');
         }
     
-        // Si le formulaire n'est pas valide ou s'il y a une erreur, afficher le formulaire
         return $this->render('requests/new.html.twig', [
           
             'form' => $form
