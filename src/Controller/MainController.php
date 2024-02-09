e3259e6605ff50d5af7a83803a64aa4ea25d7d4c
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-02-09 03:02:27 +0100

    mise en place d'une verification de sécurité pour que seul un administrateur puisse envoyer des invitations à rejoindre

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,25 +7,33 @@ use App\Entity\Notification;
 use App\Entity\UserRole;
 use App\Repository\CompanyRepository;
 use App\Repository\UserRepository;
+use App\Repository\UserRoleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class MainController extends AbstractController
 {
     #[Route('/invite/user/{companyId}/{userId}/{senderId}', name: 'app_invite_user', methods: ['GET', 'POST'])]
-    public function inviteUser(Request $request, EntityManagerInterface $entityManager, $userId, UserRepository $userRepository, $companyId, CompanyRepository $companyRepository): Response
+    public function inviteUser(Request $request, EntityManagerInterface $entityManager, $userId, UserRepository $userRepository, $companyId, CompanyRepository $companyRepository,UserRoleRepository $userRoleRepository,$senderId): Response
     {
-
+ // Vérifier si le senderId correspond à un admin online auquel cas celui ci ne peut inviter personne 
+ $senderRole = $userRoleRepository->findOneBy(['usr' => $senderId, 'state' => 'online', 'roleName' => 'admin']);
+    
+ if (!$senderRole) {
+     // Si aucune correspondance n'est trouvée l'accès  est refusé
+     throw new AccessDeniedHttpException('Vous n\'avez pas les droits pour envoyer une invitation.');
+ }
         $usr = $userRepository->findOneBy(['id' => $userId]);
         // $comp = $companyRepository->findOneBy(['id' => $companyId]);
 
         // Créer un nouveau UserRole avec un état "offline"
         $userRole = new UserRole();
         $userRole->setUsr($userId);
-        $userRole->setCompany($companyId); // Assumer que setCompany accepte un ID d'organisation
+        $userRole->setCompany($companyId); 
         $userRole->setRoleName('Votre rôle spécifique');
         $userRole->setCreatedAt(new \DateTimeImmutable());
         $userRole->setCreatedBy($userId);
