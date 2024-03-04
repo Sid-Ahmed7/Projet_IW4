74011bd101f1d5afc368797e52085acdf68c2f08
Narutino10 <ibrahim.ouahabi@outlook.fr>
2024-03-04 20:04:00 +0100

    Upload service

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -56,7 +56,7 @@ class UserController extends AbstractController
     // #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     // public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     // {
-    //     $form = $this->createForm(UserType::class, $user);
+    //     $form = $this->createForm(UserType::class);
     //     $form->handleRequest($request);
 
     //     if ($form->isSubmitted() && $form->isValid()) {
@@ -85,7 +85,7 @@ class UserController extends AbstractController
         }
     }
 
-    $form = $this->createForm(UserType::class, $user);
+    $form = $this->createForm(UserType::class);
     $form->handleRequest($request);
     
     if ($form->isSubmitted() && $form->isValid()) {
