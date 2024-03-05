453e09ab06e7dae6447e1dcf7ea74083eb999619
Narutino10 <ibrahim.ouahabi@outlook.fr>
2024-03-05 13:06:40 +0100

    correction tailwind

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
