36bf18d1d00fd21cda70caf3fc283398ea4e5b2e
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2025-05-02 11:19:10 +0200

    WIP

---
+++ migrations/Version20250501183526.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250501183526 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE devis ADD billing_details JSON DEFAULT NULL');
+        $this->addSql('ALTER TABLE invoice DROP details');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE invoice ADD details JSON DEFAULT NULL');
+        $this->addSql('ALTER TABLE devis DROP billing_details');
+    }
+}
